980d23e36d07f957c5fa89386d506b13543f9eac
traduongxuan <xuantravk@gmail.com>
2018-12-19 10:55:35 +0700

    change 2

---
+++ MFAX01V3.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFAX01V3.Core
+{
+    class Class1
+    {
+    }
+}
